135d1f5d2f121bad4dc8950df6d14b43cf989493
astalios <pro.sdebro@protonmail.com>
2021-02-20 23:38:45 +0100

    finission login

--- src/window.c
+++ src/window.c
@@ -4,7 +4,6 @@
 #include <menu.h>
 #include <form.h>
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -19,27 +18,14 @@
 #define RATIOY (LINES * 0.2)
 #define RATIOX (COLS * 0.1)
 
-void init_window_params(WIN *param_win);
-void create_box(WIN *win, bool flag);
-
-int main(int argc, char *argv[]){
-    // INIT du panel
-    WINDOW *win[4];
-    PANEL *panels[4];
-
-    // initialialisation du formulaire de connexion
-    FIELD *field_login[3];
-    FORM *form_login;
-    // init du form inscription
-   /*
-    FIELD *field_signup[4];
-    FORM *form_signup;
-    */
-    // capture claver
+//ncurses apprecie pas de generer des
+int main(void){
+    // capture claver -- ascii table
     int character;
 
     //init ecran et fenetres
     initscr();
+
     if (has_colors() == FALSE){
         perror("ERROR : ");
     } else {
@@ -54,90 +40,111 @@ int main(int argc, char *argv[]){
     keypad(stdscr, TRUE); // touches fonctions
     noecho(); // pas de texte au clavier quand on fait des input;
 
+    // INIT du panel
+    WINDOW *win_left_panel, *win_top_panel;
+    WINDOW *win_login_panel, *sub_win_login_panel;
+    //WINDOW *win_signup_panel, *sub_win_signup_panel;
+    PANEL *panels[7];
 
-    // initialisation des champs
-    // login
-    for (int i = 0; i < 2; ++i) {
-        field_login[i] = new_field(1,10,3+i,18,0,0);
-        field_opts_off(field_login[i], O_AUTOSKIP);
-    }
-    field_login[2] = NULL;
+    // initialialisation du formulaire de connexion
+    FIELD *field_login[5];
+    FORM *form_login;
+
+    // init du form inscription
+    /*
+     FIELD *field_signup[7];
+     FORM *form_signup;
+     */
+
+    // creation des windows avant entree dans les panels
+    win_left_panel = newwin(LINES, RATIOX, 0, 0); // left panel
+    win_top_panel = newwin(RATIOY, COLS, 0, 0); // top panel
+    win_login_panel = newwin(10, 50, (LINES - 10)/ 2, (COLS - 50)/2); // login_panel // body
+    sub_win_login_panel = derwin(win_login_panel, LINES / 2, COLS / 2, 3, 1); // sub_panel // form
+    //win_signup_panel = newwin(10, 50, (LINES - 10 )/ 2, (COLS - 5)/2); // password_panel
+    //sub_win_signup_panel = derwin(win_signup_panel,6,48,(LINES - 10 )/ 2, (COLS - 5)/2);
+
+    mvwprintw(win_login_panel, 1, 2, "Press F4 to quit - F2 to switch between login / sign up");
+
+    // box autour des win_*_panels
+    box(win_top_panel, 0, 0);
+    box(win_left_panel, 0, 0);
+    box(win_login_panel, 0, 0);
+    box(sub_win_login_panel, 0, 0);
+    //box(win_signup_panel, 0, 0);
+    //box(sub_win_signup_panel, 0, 0);
+
+    // initialisation des champs de login
+    field_login[0] = new_field(1,10,0,0,0,0); // label id
+    field_login[1] = new_field(1,15,0,15,0,0); // id
+    field_login[2] = new_field(1,10,2,0,0,0); // label pass
+    field_login[3] = new_field(1,15,2,15,0,0); // pass
+    field_login[4] = NULL;
+
+    //mise en place des textes labels
+    set_field_buffer(field_login[0],0,"login:");
+    set_field_buffer(field_login[2],0,"password:");
+
+    // CUSTOMISATION DES CHAMPS
+    // label id
+    set_field_opts(field_login[0], O_AUTOSKIP | O_PUBLIC | O_VISIBLE);
     // id
-    set_field_fore(field_login[0],COLOR_PAIR(1));
-    set_field_back(field_login[0],COLOR_PAIR(2));
-    set_field_type(field_login[0], TYPE_ALNUM);
-    // pass
-    field_opts_off(field_login[1], O_PUBLIC);
-    set_field_fore(field_login[1],A_UNDERLINE);
+    set_field_opts(field_login[1], O_PUBLIC | O_VISIBLE | O_ACTIVE | O_EDIT);
     set_field_back(field_login[1],A_UNDERLINE);
-    // signup
-   /* for (int i = 0; i < 3; ++i) {
-        field_signup[i] = new_field(1,10,4,18,0,0);
-        set_field_back(field_signup[i], A_BOLD);
-        field_opts_off(field_signup[i], O_AUTOSKIP);
-    }
-    field_signup[3] = NULL;
-    */
-    // creation formulaire
+    set_field_type(field_login[1], TYPE_ALNUM);
+    //label pass
+    set_field_opts(field_login[2], O_AUTOSKIP | O_PUBLIC | O_VISIBLE);
+    // pass
+    set_field_opts(field_login[3], O_VISIBLE | O_ACTIVE | O_EDIT);
+    field_opts_off(field_login[3], O_PUBLIC);
+    set_field_back(field_login[3],A_UNDERLINE);
+    set_field_type(field_login[3], TYPE_ALNUM);
+
+    //initialisation des champs signup
+    //label id
+    //id
+    //label pass
+    //pass
+    //label confirm pass
+    //confirm pass
+
+    // creation du formulaire de connexion / login.
     form_login = new_form(field_login);
     //form_signup = new_form(field_signup);
 
-    // publication formulaire
+    // integration formulaires dans fenetres
+    set_form_win(form_login, sub_win_login_panel);
+    set_form_sub(form_login, derwin(sub_win_login_panel, 18, 76, 3, 1));
+    //set_form_win(form_signup, win_signup_panel);
+    //set_form_sub(form_login, derwin(win_signup_panel, 18, 76, 1, 1));
+
+    // publication formulaire login + signup
     post_form(form_login);
-    //post_form(form_signup);
+//  post_form(form_signup);
 
-    // insersion dans la fenetre stdscr
+    //mvprintw(LINES - 2, 0, "Use UP or DOWN to switch between fields");
+    //set_current_field(form_login, field_login[0]);
     refresh();
+    wrefresh(sub_win_login_panel);
+    wrefresh(win_login_panel);
+    //wrefresh(sub_win_login_panel);
 
-    // texte pour faire joli - tutoriel
-
-    mvprintw(LINES - 2, 0, "Use UP or DOWN to switch between fields");
-    set_current_field(form_login, field_login[0]);
-    refresh();
-
-    // creation des windows avant entree dans les panels
-
-    win[0] = newwin(LINES, RATIOX, 0, 0);
-    win[1] = newwin(RATIOY, COLS, 0, 0);
-    win[2] = newwin(10, 20, (LINES - 10 )/ 2, (COLS - 5)/2);
-    win[3] = newwin(10, 20, (LINES - 10 )/ 2, (COLS - 5)/2);
-
-    mvwprintw(win[2], 4, 5, "login: ");
-    mvwprintw(win[2], 6, 5, "pass : ");
-
-    mvwprintw(win[3], 4, 5,"new login   : ");
-    mvwprintw(win[3], 5, 5,"new pass    : ");
-    mvwprintw(win[3], 6, 5,"confirm pass: ");
-
-    // box autour des panels
-    for(int i = 0; i < 4; ++i) {
-        box(win[i], 0, 0);
-    }
 
     // insertion dans les panels
     // ordre des panels -> stdscr-0-1-2-3
     // panel 2 et 3 - login / signup
-    for (int i = 0; i < 4; ++i) {
-        panels[i] = new_panel(win[i]);
-    }
+
+    panels[0] = new_panel(win_left_panel);
+    panels[1] = new_panel(win_top_panel);
+    panels[2] = NULL; new_panel(win_login_panel);
+    panels[3] = NULL; new_panel(sub_win_login_panel);
+    //panels[4] = NULL;
 
     //mise a jour des panels pour finir la creation
     update_panels();
     // affichage sur ecran
     doupdate();
-/*
-    // initialisation des fenetres
-    init_window_params(&win);
 
-    // attributs des fenetres :
-    attron(COLOR_PAIR(1));
-    printw("press F4 to exit");
-    refresh();
-    attroff(COLOR_PAIR(1));
-
-    // creation de la window
-    create_box(&win, TRUE);
-*/
     // boucle manipulation fenetre au clavier -- exercice, sert fondamentalement a rien;
     // directement copiee, j'ai deja fait le meme exo juste avant
     while((character = getch()) != KEY_F(4))
@@ -145,9 +152,12 @@ int main(int argc, char *argv[]){
         switch(character)
         {
             case KEY_LEFT:
+                form_driver(form_login, REQ_PREV_CHAR);
                 break;
             case KEY_RIGHT:
+                form_driver(form_login, REQ_NEXT_CHAR);
                 break;
+            // deplacement dans le formulaire
             case KEY_UP:
                 form_driver(form_login, REQ_PREV_FIELD);
                 form_driver(form_login, REQ_END_LINE);
@@ -156,66 +166,41 @@ int main(int argc, char *argv[]){
                 form_driver(form_login, REQ_NEXT_FIELD);
                 form_driver(form_login, REQ_END_LINE);
                 break;
+            // Supprime le caractere qui est avant le curseur
+            case KEY_BACKSPACE:
+            case 127:
+                form_driver(form_login, REQ_DEL_PREV);
+                break;
+            // Supprime le caractere qui est sur le curseur
+            case KEY_DC:
+                form_driver(form_login, REQ_DEL_CHAR);
+                break;
             default:
                 form_driver(form_login, character);
+                break;
+            // Remonter au debut ou a la fin
+            case KEY_HOME:
+                form_driver(form_login, REQ_BEG_LINE);
+                break;
+            case KEY_END:
+                form_driver(form_login, REQ_END_LINE);
                 break;
         }
     }
     // depubliage du formulaire
     unpost_form(form_login);
-
-    for (int i = 0; i < 2; ++i) {
+    //liberation memoire
+    free_form(form_login);
+    for (int i = 0; i < 4; ++i) {
         free_field(field_login[i]);
     }
     // fermeture fenetre
+    delwin(win_left_panel);
+    delwin(win_top_panel);
+    delwin(win_login_panel);
+    delwin(sub_win_login_panel);
+    //delwin(win_signup_panel);
+    //delwin(sub_win_signup_panel);
     endwin();
     return EXIT_SUCCESS;
 }
-
-void init_window_params(WIN *param_win){
-    // initialisation de la taille + centre terminal
-    // option ++ on peux meme eventuellement faire un ratio ecran
-
-    param_win->height = 5;
-    param_win->width = 10;
-    param_win->starty = (LINES - param_win->height) / 2;
-    param_win->startx = (COLS - param_win->width) / 2;
-
-    // init des bordures pour faire un joli contour
-    param_win->border.left = '|';
-    param_win->border.right = '|';
-    param_win->border.top = '-';
-    param_win->border.bottom = '-';
-    param_win->border.topLeft = '+';
-    param_win->border.topRight = '+';
-    param_win->border.bottomLeft = '+';
-    param_win->border.bottomRight = '+';
-}
-/*
-void create_box(WIN *param_win, bool flag){
-    int i, j;
-    int x, y, winHeight, winWidth;
-
-    x = param_win->startx;
-    y = param_win->starty;
-    winWidth = param_win->width;
-    winHeight = param_win->height;
-
-    if (flag == TRUE){
-        mvaddch(y, x, param_win->border.topLeft);
-        mvaddch(y, x + winWidth, param_win->border.topRight);
-        mvaddch(y + winHeight, x, param_win->border.bottomLeft);
-        mvaddch(y + winHeight, x + winWidth, param_win->border.bottomRight);
-        mvhline(y, x + 1, param_win->border.top, winWidth- 1);
-        mvhline(y + winHeight, x + 1, param_win->border.bottom, winWidth - 1);
-        mvvline(y + 1, x, param_win->border.left, winHeight -1);
-        mvvline(y + 1, x + winWidth, param_win->border.right, winHeight -1);
-    } else {
-        for (j = y; j <= y + winHeight; ++j) {
-            for (i = x; i <= x + winWidth; ++i) {
-                mvaddch(j, i , ' ');
-            }
-        }
-    }
-    refresh();
-} */
